286a0d11c99b5c30e4b025180e4977c7dfbf893c
yubrani <yag24001@byui.edu>
2025-06-12 00:03:00 -0300

    fix commit wo5

---
+++ chamber/scripts/discover.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("cards-container");
+
+  fetch("data/interest.json")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("No se pudo cargar el JSON");
+      }
+      return response.json();
+    })
+    .then(data => {
+      data.forEach(place => {
+        const card = document.createElement("section");
+        card.classList.add("place-card");
+
+        card.innerHTML = `
+          <h2>${place.name}</h2>
+          <figure>
+            <img src="${place.image}" alt="${place.title}">
+          </figure>
+          <address>${place.address}</address>
+          <p>${place.description}</p>
+          <button>Learn More</button>
+        `;
+
+        container.appendChild(card);
+      });
+    })
+    .catch(error => {
+      container.innerHTML = `<p>Error al cargar lugares: ${error.message}</p>`;
+    });
+});
